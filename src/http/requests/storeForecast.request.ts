6a6394b65e6ca78c2c1cc467cf92850a11ee4d8c
Felipe Valencia <pipeedev@gmail.com>
2022-08-30 18:29:35 -0500

    hotfix: forecast creation array len text

--- src/http/requests/storeForecast.request.ts
+++ src/http/requests/storeForecast.request.ts
@@ -2,6 +2,6 @@ import { body } from 'express-validator';
 
 export const storeForecastRequest = [
   body('name').notEmpty().withMessage('Name is required'),
-  body('items').isArray({ min: 4 }).withMessage('Items is required'),
+  body('items').isArray({ min: 4 }).withMessage('At least 4 items are required'),
   body(['items.*.name', 'items.*.value']).notEmpty(),
 ];
